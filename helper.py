35ee0a55a6f1d87b9c751ef3ad7c2a2cdeedb422
unknown <tanasun696@gmail.com>
2024-04-26 09:54:54 -0400

    finished

--- helper.py
+++
@@ -1,8 +0,0 @@
-from PyPDF2 import PdfReader
-
-def extract_pdf_text(file_path):
-    pdf_file = PdfReader(file_path)
-    text_data = ''
-    for pg in pdf_file.pages:
-        text_data += pg.extractText()
-    return text_data
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import Docx2txtLoader, PyPDFLoader
 import bs4
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
@@ -10,29 +10,30 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 import time
 import glob
-import helper
 
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
-splits = []
+documents = []
 
 load_dotenv()
 
 def loadData():
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     for file in glob.glob('data/**/*.docx', recursive=True):
         loader = Docx2txtLoader(file)
         docs = loader.load()
-        splits.append(text_splitter.split_documents(docs))
+        documents.extend(docs)
 
-    for file in glob.glob('data/**/*.pdf', recursive=True):
-        docs = helper.extract_pdf_text(file)
-        splits.append(text_splitter.split_documents(docs))
+    # for file in glob.glob('data/**/*.pdf', recursive=True):
+    #     loader = PyPDFLoader(file, extract_images=True)
+    #     docs = loader.load()
+    #     documents.append(docs)
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 loadData()
 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(documents)
 vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 retriever = vectorstore.as_retriever()
 prompt = hub.pull("rlm/rag-prompt")
@@ -43,9 +44,11 @@ rag_chain = (
     | StrOutputParser()
 )
 
+print("Training finished. Please input question\n")
 while True:
     question = input()
     if (question == "exit"):
         break
     for chunk in rag_chain.stream(question):
         print(chunk, end="", flush=True)
+    print("\n")
